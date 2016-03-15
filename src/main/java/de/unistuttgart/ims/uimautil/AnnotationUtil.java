9057204740d506efc34fbbde7e6076fbd6302978
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-03-15 19:49:15 +0100

    added documentation and @since annotations

--- src/main/java/de/unistuttgart/ims/uimautil/AnnotationUtil.java
+++ src/main/java/de/unistuttgart/ims/uimautil/AnnotationUtil.java
@@ -8,6 +8,12 @@ import org.apache.commons.lang.ArrayUtils;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.tcas.Annotation;
 
+/**
+ * This class contains static method to manipulate annotations.
+ *
+ * @author reiterns
+ *
+ */
 public class AnnotationUtil {
 
 	static Pattern pattern = null;
@@ -16,10 +22,10 @@ public class AnnotationUtil {
 	 * trims the annotated text. Similar to {@link String#trim()}, this method
 	 * moves the begin and end indexes towards the middle as long as there is
 	 * whitespace.
-	 * 
+	 *
 	 * The method throws a ArrayIndexOutOfBoundsException if the entire
 	 * annotation consists of whitespace.
-	 * 
+	 *
 	 * @param annotation
 	 *            The annotation to trim
 	 * @param ws
@@ -27,10 +33,12 @@ public class AnnotationUtil {
 	 * @param <T>
 	 *            The annotation type
 	 * @return The trimmed annotation (not a copy)
+	 * @since 0.4.1
 	 */
 	public static <T extends Annotation> T trim(T annotation, char... ws) {
-		char[] s = annotation.getCoveredText().toCharArray();
-		if (s.length == 0) return annotation;
+		final char[] s = annotation.getCoveredText().toCharArray();
+		if (s.length == 0)
+			return annotation;
 
 		int b = 0;
 		while (ArrayUtils.contains(ws, s[b])) {
@@ -47,16 +55,18 @@ public class AnnotationUtil {
 	}
 
 	/**
-	 * 
-	 * 
-	 * This method first checks whether the string contains whitespace at
-	 * all. See also {@link #trim(Annotation, char...) }
-	 * 
+	 *
+	 *
+	 * This method first checks whether the string contains whitespace at all.
+	 * See also {@link #trim(Annotation, char...) }
+	 *
 	 * @param annotation
 	 *            The annotation to trim
 	 * @param <T>
 	 *            the annotation type
 	 * @return the trimmed annotation
+	 * @since 0.4.1
+	 *
 	 */
 	public static <T extends Annotation> T trim(T annotation) {
 		if (pattern == null) {
@@ -72,14 +82,15 @@ public class AnnotationUtil {
 	 * trims an entire collection of annotations. Beware: directly trimming the
 	 * result of {@link JCasUtil#select(org.apache.uima.jcas.JCas, Class)}
 	 * throws a {@link ConcurrentModificationException}.
-	 * 
+	 *
 	 * @param annotations
 	 *            The annotations you want to trim
 	 * @param <T>
 	 *            the annotation type
+	 * @since 0.4.1
 	 */
 	public static <T extends Annotation> void trim(Collection<T> annotations) {
-		for (T anno : annotations) {
+		for (final T anno : annotations) {
 			trim(anno, ' ', '\n', '\t', '\r', '\f');
 		}
 	}
@@ -87,7 +98,7 @@ public class AnnotationUtil {
 	/**
 	 * Moves the begin-index as long as a character contain in the array is at
 	 * the beginning.
-	 * 
+	 *
 	 * @param annotation
 	 *            the annotation to be trimmed
 	 * @param ws
@@ -95,10 +106,12 @@ public class AnnotationUtil {
 	 * @param <T>
 	 *            the annotation type
 	 * @return the trimmed annotation
+	 * @since 0.4.1
 	 */
 	public static <T extends Annotation> T trimFront(T annotation, char... ws) {
-		char[] s = annotation.getCoveredText().toCharArray();
-		if (s.length == 0) return annotation;
+		final char[] s = annotation.getCoveredText().toCharArray();
+		if (s.length == 0)
+			return annotation;
 
 		int b = 0;
 		while (ArrayUtils.contains(ws, s[b])) {
