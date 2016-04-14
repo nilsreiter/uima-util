99e492d6d9090bebc0afc470f7cafec1c6877e98
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-04-14 17:39:16 +0200

    Added option to produce overlapping annotations

--- src/main/java/de/unistuttgart/ims/uimautil/WindowAnnotator.java
+++ src/main/java/de/unistuttgart/ims/uimautil/WindowAnnotator.java
@@ -1,5 +1,7 @@
 package de.unistuttgart.ims.uimautil;
 
+import java.util.LinkedList;
+
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
@@ -12,7 +14,11 @@ import org.apache.uima.resource.ResourceInitializationException;
 
 /**
  * This component adds window annotations of type PARAM_TARGET_ANNOTATION over
- * PARAM_WINDOW_SIZE base annotations.
+ * PARAM_WINDOW_SIZE base annotations. The last window might be shorter than the
+ * specified window size. By default, the component generates non-overlapping
+ * windows. If the parameter PARAM_OVERLAPS is set to true, overlapping windows
+ * will be annotated. This option also ensures that every window is exactly the
+ * right amount of sub annotations long.
  *
  * @author reiterns
  * @since 0.4.3
@@ -33,7 +39,7 @@ public class WindowAnnotator extends JCasAnnotator_ImplBase {
 	@ConfigurationParameter(name = PARAM_WINDOW_SIZE, mandatory = true)
 	int windowSize;
 
-	@ConfigurationParameter(name = PARAM_OVERLAPS, mandatory = false, defaultValue = "true")
+	@ConfigurationParameter(name = PARAM_OVERLAPS, mandatory = false, defaultValue = "false")
 	boolean overlappingWindows = false;
 
 	Class<? extends Annotation> targetAnnotation = null;
@@ -73,16 +79,29 @@ public class WindowAnnotator extends JCasAnnotator_ImplBase {
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
 		int i = 0;
 		int b = 0;
+		LinkedList<Annotation> window = null;
+		if (overlappingWindows) {
+			window = new LinkedList<Annotation>();
+		}
 		for (final Annotation anno : JCasUtil.select(jcas, baseAnnotation)) {
-			i++;
-			if ((i % windowSize) == 0) {
-				AnnotationFactory.createAnnotation(jcas, b, anno.getEnd(), targetAnnotation);
-			}
-			if ((i % windowSize) == 1) {
-				b = anno.getBegin();
+			if (overlappingWindows) {
+				window.add(anno);
+				if (window.size() >= windowSize) {
+					final Annotation firstAnno = window.pop();
+					AnnotationFactory.createAnnotation(jcas, firstAnno.getBegin(), anno.getEnd(), targetAnnotation);
+				}
+			} else {
+				i++;
+				if ((i % windowSize) == 0) {
+					AnnotationFactory.createAnnotation(jcas, b, anno.getEnd(), targetAnnotation);
+				}
+				if ((i % windowSize) == 1) {
+					b = anno.getBegin();
+				}
 			}
 		}
-		AnnotationFactory.createAnnotation(jcas, b, jcas.getDocumentText().length(), targetAnnotation);
+		if (!overlappingWindows)
+			AnnotationFactory.createAnnotation(jcas, b, jcas.getDocumentText().length(), targetAnnotation);
 	}
 
 }
