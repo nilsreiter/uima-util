f7513ba3a0fe2fee129a61964f31c65199c41d93
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-06-17 13:18:56 +0200

    added basic Trie implementation

---
+++ src/main/java/de/unistuttgart/ims/uimautil/trie/Trie.java
@@ -0,0 +1,46 @@
+package de.unistuttgart.ims.uimautil.trie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trie<T> {
+	private TrieNode<T> root;
+
+	/**
+	 * Constructor
+	 */
+	public Trie() {
+		root = new TrieNode<T>();
+	}
+
+	/**
+	 * Adds a word to the Trie
+	 * 
+	 * @param word
+	 */
+	public void addWord(List<T> word) {
+		root.addWord(word);
+	}
+
+	/**
+	 * Get the words in the Trie with the given prefix
+	 * 
+	 * @param prefix
+	 * @return a List containing String objects containing the words in the Trie
+	 *         with the given prefix.
+	 */
+	public List<List<T>> getWords(List<T> prefix) {
+		// Find the node which represents the last letter of the prefix
+		TrieNode<T> lastNode = root;
+		for (int i = 0; i < prefix.size(); i++) {
+			lastNode = lastNode.getNode(prefix.get(i));
+
+			// If no node matches, then no words exist, return empty list
+			if (lastNode == null)
+				return new ArrayList<List<T>>();
+		}
+
+		// Return the words which eminate from the last node
+		return lastNode.getWords();
+	}
+}
