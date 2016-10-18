58f45f5c519cc12de5fa8448f54b541c5d9b33d1
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-10-18 14:40:10 +0200

    we can export and configure primitive feature paths

--- src/main/java/de/unistuttgart/ims/uimautil/CoNLLStyleExporter.java
+++ src/main/java/de/unistuttgart/ims/uimautil/CoNLLStyleExporter.java
@@ -11,30 +11,46 @@ import java.util.List;
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.uima.UIMAException;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.cas.FeaturePath;
+import org.apache.uima.cas.Type;
 import org.apache.uima.fit.component.JCasConsumer_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.descriptor.OperationalProperties;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.metadata.TypeDescription;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
 
 import de.unistuttgart.ims.uimautil.export.ExportEntry;
+import de.unistuttgart.ims.uimautil.export.FeaturePathExportEntry;
 
 @OperationalProperties(multipleDeploymentAllowed = false)
 public class CoNLLStyleExporter extends JCasConsumer_ImplBase {
 
 	public static final String PARAM_OUTPUT_FILE = "Output File";
-
 	public static final String PARAM_ANNOTATION_CLASS = "Annotation Class";
+	public static final String PARAM_FEATURE_PATHS = "Feature Paths";
+	public static final String PARAM_COLUMN_LABELS = "Feature Path Labels";
 
 	@ConfigurationParameter(name = PARAM_OUTPUT_FILE, mandatory = true)
 	File outputFile;
 
 	@ConfigurationParameter(name = PARAM_ANNOTATION_CLASS, defaultValue = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token")
 	String annotationClassName;
+
+	@ConfigurationParameter(name = PARAM_FEATURE_PATHS, mandatory = false)
+	String[] paths = new String[] {};
+
+	@ConfigurationParameter(name = PARAM_COLUMN_LABELS, mandatory = false)
+	String[] labels = new String[] {};
 
 	CSVPrinter csvPrinter;
 
@@ -53,7 +69,42 @@ public class CoNLLStyleExporter extends JCasConsumer_ImplBase {
 			throw new ResourceInitializationException(e);
 		}
 
+		try {
+			annotationClass = (Class<? extends Annotation>) Class.forName(annotationClassName);
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+			throw new ResourceInitializationException(e1);
+		}
+
 		eelist = new LinkedList<ExportEntry>();
+
+		JCas jcas;
+		TypeSystemDescription tsd;
+		try {
+			jcas = JCasFactory.createJCas();
+			tsd = TypeSystemDescriptionFactory.createTypeSystemDescription();
+		} catch (UIMAException e) {
+			throw new ResourceInitializationException(e);
+		}
+
+		TypeDescription td = tsd.getType(annotationClass.getName());
+		Type type = jcas.getTypeSystem().getType(td.getName());
+
+		for (int i = 0; i < paths.length; i++) {
+			String path = paths[i];
+			FeaturePath fp = jcas.createFeaturePath();
+			try {
+				fp.initialize(path);
+				fp.typeInit(type);
+				if (labels.length > i) {
+					eelist.add(new FeaturePathExportEntry(fp, labels[i]));
+				} else {
+					eelist.add(new FeaturePathExportEntry(fp));
+				}
+			} catch (CASException e) {
+				e.printStackTrace();
+			}
+		}
 
 		// assemble the header
 		List<Object> header = new LinkedList<Object>();
---
+++ src/test/java/de/unistuttgart/ims/uimautil/TestCoNLLStyleExporter.java
@@ -0,0 +1,51 @@
+package de.unistuttgart.ims.uimautil;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.AnnotationFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+
+public class TestCoNLLStyleExporter {
+	JCas jcas;
+
+	@Before
+	public void setUp() throws UIMAException {
+		jcas = JCasFactory.createJCas();
+		jcas.setDocumentText(
+				"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.");
+		Token[] tokens = new Token[] { AnnotationFactory.createAnnotation(jcas, 0, 5, Token.class),
+				AnnotationFactory.createAnnotation(jcas, 6, 11, Token.class) };
+		POS[] poss = new POS[] { AnnotationFactory.createAnnotation(jcas, 0, 5, POS.class),
+				AnnotationFactory.createAnnotation(jcas, 6, 11, POS.class) };
+		poss[0].setPosValue("A");
+		poss[1].setPosValue("B");
+		tokens[0].setPos(poss[0]);
+		tokens[1].setPos(poss[1]);
+
+	}
+
+	@Test(expected = ResourceInitializationException.class)
+	public void testExporter1() throws AnalysisEngineProcessException, ResourceInitializationException {
+		SimplePipeline.runPipeline(jcas, AnalysisEngineFactory.createEngineDescription(CoNLLStyleExporter.class));
+	}
+
+	@Test
+	public void testExporter2() throws AnalysisEngineProcessException, ResourceInitializationException {
+		SimplePipeline.runPipeline(jcas,
+				AnalysisEngineFactory.createEngineDescription(CoNLLStyleExporter.class,
+						CoNLLStyleExporter.PARAM_ANNOTATION_CLASS, Token.class, CoNLLStyleExporter.PARAM_OUTPUT_FILE,
+						"target/conll2.csv", CoNLLStyleExporter.PARAM_FEATURE_PATHS,
+						new String[] { "/:coveredText()", "/pos/PosValue" }, CoNLLStyleExporter.PARAM_COLUMN_LABELS,
+						new String[] { "surface", "pos" }));
+	}
+
+}
