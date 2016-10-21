b5b69318dd9460de65af4d9a78e2223c9e48ca5e
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-10-21 12:18:48 +0200

    moved heavy lifting in another class that can be used without a pipeline

--- src/main/java/de/unistuttgart/ims/uimautil/CoNLLCasConsumer.java
+++ src/main/java/de/unistuttgart/ims/uimautil/CoNLLCasConsumer.java
@@ -6,10 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.commons.compress.utils.IOUtils;
@@ -21,28 +17,14 @@ import org.apache.commons.csv.CSVPrinter;
 import org.apache.uima.UIMAException;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.CASException;
-import org.apache.uima.cas.Feature;
-import org.apache.uima.cas.FeaturePath;
 import org.apache.uima.cas.Type;
 import org.apache.uima.fit.component.JCasConsumer_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.descriptor.OperationalProperties;
 import org.apache.uima.fit.factory.JCasFactory;
-import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
-import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
-import org.apache.uima.resource.metadata.TypeDescription;
-import org.apache.uima.resource.metadata.TypeSystemDescription;
-
-import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
-import de.unistuttgart.ims.uimautil.export.CoveredColumn;
-import de.unistuttgart.ims.uimautil.export.Column;
-import de.unistuttgart.ims.uimautil.export.FeaturePathColumn;
-import de.unistuttgart.ims.uimautil.export.PrimitiveColumn;
 
 @OperationalProperties(multipleDeploymentAllowed = false)
 public class CoNLLCasConsumer extends JCasConsumer_ImplBase {
@@ -69,25 +51,15 @@ public class CoNLLCasConsumer extends JCasConsumer_ImplBase {
 
 	CSVPrinter csvPrinter;
 
-	List<Column> columnList;
-
-	Class<? extends Annotation> annotationClass;
-	Class<? extends Annotation> coveredAnnotationClass = null;
 	CombinedConfiguration config;
 
 	Type type;
+	CoNLLExport exporter;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public void initialize(final UimaContext context) throws ResourceInitializationException {
 		super.initialize(context);
-
-		try {
-			csvPrinter = new CSVPrinter(new FileWriter(outputFile), CSVFormat.DEFAULT);
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new ResourceInitializationException(e);
-		}
 
 		INIConfiguration defaultConfig = new INIConfiguration();
 		INIConfiguration serverConfig = new INIConfiguration();
@@ -123,6 +95,8 @@ public class CoNLLCasConsumer extends JCasConsumer_ImplBase {
 		config = new CombinedConfiguration(new OverrideCombiner());
 		config.addConfiguration(serverConfig);
 		config.addConfiguration(defaultConfig);
+		Class<? extends Annotation> annotationClass = null;
+		Class<? extends Annotation> coveredAnnotationClass = null;
 
 		Class<?> cl = null;
 		try {
@@ -146,282 +120,43 @@ public class CoNLLCasConsumer extends JCasConsumer_ImplBase {
 			coveredAnnotationClass = (Class<? extends Annotation>) cl;
 
 		JCas jcas;
-		TypeSystemDescription tsd;
+
 		try {
 			jcas = JCasFactory.createJCas();
-			tsd = TypeSystemDescriptionFactory.createTypeSystemDescription();
 		} catch (UIMAException e) {
 			throw new ResourceInitializationException(e);
 		}
 
-		TypeDescription td = tsd.getType(annotationClass.getName());
-		type = jcas.getTypeSystem().getType(td.getName());
-		Type coveredType = null;
-		if (coveredAnnotationClass != null) {
-			coveredType = jcas.getTypeSystem().getType(coveredAnnotationClass.getName());
-		}
-
-		columnList = getColumns(jcas, type, coveredType);
-
-		String confKey = type.getName().replaceAll("\\.", "..") + ".fixed";
-		String[] confValues = config.getString(confKey, "").split(",");
-		String[] confValueLabels = config.getString(confKey + "_labels", "").split(",");
-
-		for (int i = 0; i < confValues.length; i++) {
-			String confEntry = confValues[i].trim();
-			String confEntryLabel = confValueLabels[i].trim();
-			if (confEntry.equalsIgnoreCase("DocumentId")) {
-				columnList.add(0, new Column(new String[] { confEntryLabel }) {
-
-					@Override
-					public Object getValue(Annotation a) {
-						try {
-							return DocumentMetaData.get(a.getCAS()).getDocumentId();
-						} catch (Exception e) {
-							return "";
-						}
-					}
-
-					@Override
-					public boolean isMultiplying() {
-						return false;
-					}
-				});
-			} else if (confEntry.equalsIgnoreCase("Length")) {
-				columnList.add(0, new Column(new String[] { confEntryLabel }) {
-
-					@Override
-					public Object getValue(Annotation a) {
-						try {
-							JCas jcas = a.getCAS().getJCas();
-							return JCasUtil.select(jcas, Token.class).size();
-						} catch (CASException e) {
-							e.printStackTrace();
-							return 0;
-						}
-
-					}
-
-					@Override
-					public boolean isMultiplying() {
-						return false;
-					}
-				});
-			}
-		}
-
-		// assemble the header
-		List<Object> header = new LinkedList<Object>();
-		for (Column ee : columnList) {
-			for (String s : ee.getLabel()) {
-				header.add(s);
-			}
-		}
-
-		// print the header
+		exporter = new CoNLLExport();
 		try {
-			csvPrinter.printRecord(header);
-		} catch (IOException e) {
+			exporter.init(config, jcas, annotationClass, coveredAnnotationClass);
+		} catch (UIMAException e) {
 			e.printStackTrace();
 			throw new ResourceInitializationException(e);
+
 		}
 
 	}
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
-		Collection<? extends Annotation> annotationList = JCasUtil.select(jcas, annotationClass);
-
-		// print entries
-		for (Annotation a : annotationList) {
-
-			for (List<Object> l : printFeatureValues(a, columnList.iterator())) {
-				try {
-					csvPrinter.printRecord(l);
-				} catch (IOException e) {
-					e.printStackTrace();
-					throw new AnalysisEngineProcessException(e);
-				}
-			}
-		}
+		exporter.convert(jcas);
 	}
 
 	@Override
 	public void collectionProcessComplete() throws AnalysisEngineProcessException {
 		try {
+			csvPrinter = new CSVPrinter(new FileWriter(outputFile), CSVFormat.DEFAULT);
+			for (List<Object> row : exporter.getResult()) {
+				csvPrinter.printRecord(row);
+			}
+
 			csvPrinter.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new AnalysisEngineProcessException(e);
 		}
 		IOUtils.closeQuietly(csvPrinter);
-	}
-
-	private ArrayList<ArrayList<Object>> printFeatureValues(Annotation a, Iterator<Column> eelist) {
-		ArrayList<ArrayList<Object>> r = new ArrayList<ArrayList<Object>>();
-		r.add(new ArrayList<Object>());
-
-		while (eelist.hasNext()) {
-			Column ee = eelist.next();
-			Object value = ee.getValue(a);
-			if (ee.isMultiplying()) {
-				ArrayList<ArrayList<ArrayList<Object>>> clones = new ArrayList<ArrayList<ArrayList<Object>>>();
-				Object[] vals = (Object[]) value;
-				for (int j = 1; j < vals.length; j++) {
-					clones.add(deepClone(r));
-				}
-
-				for (List<Object> l : r) {
-					if (vals.length == 0) {
-						l.add("");
-					} else
-						for (Object o : (Object[]) vals[0]) {
-							l.add(o);
-						}
-				}
-
-				for (int j = 1; j < vals.length; j++) {
-					for (ArrayList<Object> l : clones.get(j - 1)) {
-						for (Object o : (Object[]) vals[j]) {
-							l.add(o);
-						}
-					}
-					r.addAll(clones.get(j - 1));
-				}
-			} else {
-				for (List<Object> l : r) {
-					l.add(value);
-				}
-			}
-		}
-		return r;
-
-	}
-
-	private String[] getFeaturePathsForType(Type type) {
-		String confKey = type.getName().replaceAll("\\.", "..") + ".paths";
-		String confEntry = config.getString(confKey, null);
-		if (confEntry != null && !confEntry.isEmpty())
-			return confEntry.split(",");
-		else
-			return new String[0];
-
-	}
-
-	private String[] getColumnHeadersForType(Type type) {
-		String confKey = type.getName().replaceAll("\\.", "..") + ".labels";
-		String confEntry = config.getString(confKey, null);
-		if (confEntry != null && !confEntry.isEmpty())
-			return confEntry.split(",");
-		else
-			return new String[0];
-
-	}
-
-	@SuppressWarnings("unchecked")
-	private ArrayList<ArrayList<Object>> deepClone(ArrayList<ArrayList<Object>> list) {
-		ArrayList<ArrayList<Object>> ret = new ArrayList<ArrayList<Object>>();
-		for (ArrayList<Object> l : list) {
-			ret.add((ArrayList<Object>) l.clone());
-		}
-		return ret;
-	}
-
-	@SuppressWarnings("unchecked")
-	protected List<Column> getColumns(JCas jcas, Type type, Type coveredType) {
-		List<Column> eelist = new LinkedList<Column>();
-
-		for (Feature fd : type.getFeatures()) {
-			if (fd.getRange().isPrimitive()) {
-				PrimitiveColumn pee = new PrimitiveColumn(
-						jcas.getTypeSystem().getFeatureByFullName(fd.getName()));
-				eelist.add(pee);
-			}
-		}
-		String[] paths = this.getFeaturePathsForType(type);
-		String[] labels = this.getColumnHeadersForType(type);
-		for (int i = 0; i < paths.length; i++) {
-			String path = paths[i];
-			FeaturePath fp = jcas.createFeaturePath();
-			try {
-				fp.initialize(path);
-				fp.typeInit(type);
-				if (labels.length > i) {
-					eelist.add(new FeaturePathColumn(fp, labels[i]));
-				} else {
-					eelist.add(new FeaturePathColumn(fp));
-				}
-			} catch (CASException e) {
-				e.printStackTrace();
-
-			}
-		}
-		String[] covTypes = this.getCoveringsForType(type);
-		for (int j = 0; j < covTypes.length; j++) {
-			paths = getFeaturePathsForType(jcas.getTypeSystem().getType(covTypes[j]));
-			labels = getColumnHeadersForType(jcas.getTypeSystem().getType(covTypes[j]));
-			FeaturePath[] path = new FeaturePath[paths.length];
-			for (int i = 0; i < path.length; i++) {
-				path[i] = jcas.createFeaturePath();
-				try {
-					labels[i] = Class.forName(covTypes[j]).getSimpleName() + "/" + labels[i];
-				} catch (ClassNotFoundException e1) {
-					e1.printStackTrace();
-				}
-				try {
-					path[i].initialize(paths[i]);
-					path[i].typeInit(jcas.getTypeSystem().getType(covTypes[j]));
-				} catch (CASException e) {
-					e.printStackTrace();
-				}
-			}
-			try {
-				Column ee = new CoveredColumn((Class<? extends Annotation>) Class.forName(covTypes[j]), path);
-				ee.setLabel(labels);
-				eelist.add(ee);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-
-		if (coveredType != null)
-
-		{
-			labels = getColumnHeadersForType(coveredType);
-			paths = getFeaturePathsForType(coveredType);
-			FeaturePath[] path = new FeaturePath[paths.length];
-			for (int i = 0; i < path.length; i++) {
-				path[i] = jcas.createFeaturePath();
-				try {
-					labels[i] = coveredType.getShortName() + "/" + labels[i];
-					path[i].initialize(paths[i]);
-					path[i].typeInit(coveredType);
-				} catch (CASException e) {
-					e.printStackTrace();
-				}
-			}
-			try {
-				Column ee = new CoveredColumn(
-						(Class<? extends Annotation>) Class.forName(coveredType.getName()), path);
-				ee.setLabel(labels);
-				eelist.add(ee);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-
-		}
-		return eelist;
-
-	}
-
-	protected String[] getCoveringsForType(Type type) {
-		String confKey = type.getName().replaceAll("\\.", "..") + ".covered";
-		String confEntry = config.getString(confKey, null);
-		if (confEntry != null && !confEntry.isEmpty())
-			return confEntry.split(",");
-		else
-			return new String[0];
-
 	}
 
 }
